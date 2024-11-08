661c26864f467b9f0876741cb4ececfc55c1933f
giancarlogiorgiocroce <giancarlo.giorgio.croce@gmail.com>
2024-11-08 23:09:50 +0100

    new file:   index.php new file:   script.js Commit iniziale

---
+++ index.php
@@ -0,0 +1,32 @@
+<?php 
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Monitoring Object Position System</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <main class="content">
+    <h1>Hi this is the M.O.P.S. app, please scan something!</h1>
+    <div class="scanner-ct">
+      
+      <button id="scan">You are scanning</button>
+    </div>
+  </main>
+
+  <div class="hidden">
+    <button type="submit" id="send"></button>
+    <form action="" method="post">
+      <input type="text" value="" id="">
+      <input type="text" value="" id="time">
+    </form>
+  </div>
+  <script src="/script.js"></script>
+</body>
+</html>
